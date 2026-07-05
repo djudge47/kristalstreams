2e1f7a5953456cf3e9315d52f14811fe531116d9
Donald <djudge47@yahoo.com>
2026-07-05 11:28:33 -0400

    Use the exact uploaded MLB image in the card

--- src/assets/mlbAllStarImage.ts
+++ src/assets/mlbAllStarImage.ts
@@ -1,6 +1,11 @@
-import mlbAllStarPart1 from './mlbAllStar/part1';
-import mlbAllStarPart2 from './mlbAllStar/part2';
+import part1 from './mlbUploaded/part1';
+import part2 from './mlbUploaded/part2';
+import part3 from './mlbUploaded/part3';
+import part4 from './mlbUploaded/part4';
+import part5 from './mlbUploaded/part5.txt?raw';
+import part6 from './mlbUploaded/part6';
 
-const mlbAllStarImage = `data:image/jpeg;base64,${mlbAllStarPart1}${mlbAllStarPart2}`;
+const base64 = `${part1}${part2}${part3}${part4}${part5}${part6}`.replace(/\s/g, '');
+const mlbAllStarImage = `data:image/jpeg;base64,${base64}`;
 
 export default mlbAllStarImage;
