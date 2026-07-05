e3a9274ba891e5056658db75a798e52159e24a9e
Donald <djudge47@yahoo.com>
2026-07-05 10:45:54 -0400

    Fix MLB image by loading both image parts

--- src/assets/mlbAllStarImage.ts
+++ src/assets/mlbAllStarImage.ts
@@ -1,5 +1,6 @@
-import mlbAllStarImageData from './mlbAllStar/part1';
+import mlbAllStarPart1 from './mlbAllStar/part1';
+import mlbAllStarPart2 from './mlbAllStar/part2';
 
-const mlbAllStarImage = `data:image/jpeg;base64,${mlbAllStarImageData}`;
+const mlbAllStarImage = `data:image/jpeg;base64,${mlbAllStarPart1}${mlbAllStarPart2}`;
 
 export default mlbAllStarImage;
