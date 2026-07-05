5308d09de00d1382d9179af468eecb9b1b66d63f
Donald <djudge47@yahoo.com>
2026-07-04 22:40:24 -0400

    Assemble verified UFC 329 JPEG artwork

---
+++ src/assets/ufc329Image.ts
@@ -0,0 +1,11 @@
+import cardPart1 from './ufc329/cardPart1';
+import cardPart2 from './ufc329/cardPart2';
+import cardPart3 from './ufc329/cardPart3';
+import cardPart4 from './ufc329/cardPart4';
+import cardPart5 from './ufc329/cardPart5';
+import cardPart6 from './ufc329/cardPart6';
+import cardPart7 from './ufc329/cardPart7';
+
+const ufc329Image = `data:image/jpeg;base64,${cardPart1}${cardPart2}${cardPart3}${cardPart4}${cardPart5}${cardPart6}${cardPart7}`;
+
+export default ufc329Image;
