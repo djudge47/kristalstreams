168706ace8a3305455f0ada48661557b5fc4d69b
Donald <djudge47@yahoo.com>
2026-07-04 20:50:28 -0400

    Assemble WNBA All-Star card artwork

---
+++ src/assets/wnbaAllStarImage.ts
@@ -0,0 +1,6 @@
+import wnbaAllStarPart1 from './wnbaAllStarPart1';
+import wnbaAllStarPart2 from './wnbaAllStarPart2';
+
+const wnbaAllStarImage = `data:image/jpeg;base64,${wnbaAllStarPart1}${wnbaAllStarPart2}`;
+
+export default wnbaAllStarImage;
