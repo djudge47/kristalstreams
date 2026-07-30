de798fbb684ad40a710c2751688182f349a0cf84
Donald <djudge47@yahoo.com>
2026-07-30 15:51:08 -0400

    Hide show it in action section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,5 @@
 import React, { lazy, Suspense, useEffect, useState } from 'react';
 import Hero from '../components/Hero';
-import DemoReel from '../components/DemoReel';
 
 const TrendingSlider = lazy(() => import('../components/TrendingSlider'));
 const Features = lazy(() => import('../components/Features'));
@@ -18,7 +17,6 @@ const Home: React.FC = () => {
   return (
     <>
       <Hero />
-      <DemoReel />
       {showDeferred && (
         <Suspense fallback={<div className="h-20" />}>
           <TrendingSlider />
